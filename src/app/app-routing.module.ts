d220ac0c1005851ae105fbf4a7654796e759bddb
FusedBaggins <faguiar408@gmail.com>
2021-01-16 13:07:30 -0300

    criação de rota para o loginComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './core/components/login/login.component';
 
 const routes: Routes = [
-  {path: '', 
-  loadChildren: () => import('./game/game.module').then(m => m.GameModule)}
+  { path: '', loadChildren: () => import('./game/game.module').then(m => m.GameModule) },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
