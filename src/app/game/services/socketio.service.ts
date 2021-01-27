f0c5580cef1895d36b2bffcfb39408d07a8da629
Gabriel Müller <gabrielmuller2501@gmail.com>
2021-01-26 22:30:10 -0300

    Alteração do socketioService para funcionar chat

--- src/app/game/services/socketio.service.ts
+++ src/app/game/services/socketio.service.ts
@@ -9,21 +9,44 @@ export class SocketioService {
 
   private socket: any;
   private users$: Subject<[]>;
+  private messages$: Subject<any[]>;
+  private messageList: any[];
+  private _room!: string;
+  private _myUsername!: string;
 
   constructor() {
     this.users$ = new Subject();
+    this.messages$ = new Subject();
+    this.messageList = [];
   }
 
   setupSocketConnection(room: string, username: string) {
     this.socket = io("http://localhost:8080");
     this.socket.emit('new_user', room, username);
+    this._room = room;
+    this._myUsername = username;
 
-    this.socket.on("update_users", (data: any) => {
+    this.socket.on('update_users', (data: any) => {
       this.users$.next(data.users);
     });
+
+    this.socket.on('message', (data: any) => {
+      this.messageList.push({ user: data.user, text: data.text });
+      this.messages$.next(this.messageList);
+    });
+  }
+
+  emitEvent(event: string, ...args: any): void {
+    args[0].room = this._room;
+    args[0].username = this._myUsername;
+    this.socket.emit(event, ...args);
   }
 
   getUsers() {
     return this.users$;
+  }
+
+  getMessages() {
+    return this.messages$;
   }
 }
