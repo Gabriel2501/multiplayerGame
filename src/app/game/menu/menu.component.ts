58fb81ff5ab50d6e78b77ce7341bf72295a9a993
Garren Souza <contato.garren@gmail.com>
2021-02-04 22:10:00 -0300

    adding language support

--- src/app/game/menu/menu.component.ts
+++ src/app/game/menu/menu.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { Subscription } from 'rxjs';
+import { LanguageService } from 'src/app/core/services/language.service';
 
 import { AjudaComponent } from './ajuda/ajuda.component';
 
@@ -8,24 +10,31 @@ import { AjudaComponent } from './ajuda/ajuda.component';
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent implements OnInit, OnDestroy {
 
-  idiomas: any[];
+  languages!: string[];
+  selectedLanguage!: string;
+  languageNotifierSubscription!: Subscription;
 
-  constructor(public dialog: MatDialog) {
-    this.idiomas = ["BR", "EN", "ES"];
+  constructor(public dialog: MatDialog, public languageService: LanguageService) {
   }
 
   ngOnInit(): void {
+    this.languageNotifierSubscription = this.languageService.getLanguageNotifier().subscribe(()=>{
+      this.selectedLanguage = this.languageService.getSelectedLanguage();
+    });
+    this.languages = this.languageService.getLanguages();
+  }
+
+  ngOnDestroy():void{
+    this.languageNotifierSubscription.unsubscribe();
   }
 
   exibirAjuda() {
     this.dialog.open(AjudaComponent, {});
   }
 
-  selecionarIdioma(idioma: string): void {
-    // Preparar arquivo de Settings, talvez?
-    // A partir daqui teria que alterar o idioma
-    console.log(idioma);
+  selectLanguage(newLanguage: string): void {
+    this.languageService.updateLanguage(newLanguage);
   }
 }
