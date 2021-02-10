da12617734c6fedd03fd27a01cdd4c7dfc6ba90b
Gabriel Müller <gabrielmuller2501@gmail.com>
2021-02-09 21:41:48 -0300

    Conflict resolved

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -1,19 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
 import { MatRippleModule } from '@angular/material/core';
-=======
-import {MatDividerModule} from '@angular/material/divider';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatListModule} from '@angular/material/list';
-import {MatMenuModule} from '@angular/material/menu';
->>>>>>> 0a1023ea6458038b3aebdf1fa5d8db1879e940d4
+import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [],
@@ -28,11 +25,8 @@ import {MatMenuModule} from '@angular/material/menu';
     MatInputModule,
     MatDialogModule,
     MatListModule,
-<<<<<<< HEAD
     MatRippleModule,
-=======
     MatMenuModule
->>>>>>> 0a1023ea6458038b3aebdf1fa5d8db1879e940d4
   ],
   exports: [
     MatDividerModule,
@@ -43,12 +37,9 @@ import {MatMenuModule} from '@angular/material/menu';
     MatFormFieldModule,
     MatInputModule,
     MatDialogModule,
-<<<<<<< HEAD
-    MatRippleModule,
-=======
     MatListModule,
+    MatRippleModule,
     MatMenuModule
->>>>>>> 0a1023ea6458038b3aebdf1fa5d8db1879e940d4
   ]
 })
 export class AngularMaterialModule { }
