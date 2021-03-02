2ca9bac95582b1848aeb0277fcaa08c75621b088
FusedBaggins <faguiar408@gmail.com>
2021-03-02 12:38:03 -0300

    inject LogService in component

--- src/app/utils/components/log/log.component.ts
+++ src/app/utils/components/log/log.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LogService } from '../../services/log.service';
 
 @Component({
   selector: 'app-log',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _logService: LogService
+  ) { }
 
   ngOnInit(): void {
   }
