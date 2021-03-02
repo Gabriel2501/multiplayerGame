bfba6c88c669b4cc0b9890f5364c44f25471d003
FusedBaggins <faguiar408@gmail.com>
2021-03-02 12:39:18 -0300

    inject log and chat service on webSocket service

--- src/app/game/services/web-socket.service.ts
+++ src/app/game/services/web-socket.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { ChatService } from 'src/app/utils/services/chat.service';
+import { LogService } from 'src/app/utils/services/log.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebSocketService {
 
-  constructor() { }
+  constructor(
+    private _logService: LogService,
+    private _chatService: ChatService
+  ) { }
 }
